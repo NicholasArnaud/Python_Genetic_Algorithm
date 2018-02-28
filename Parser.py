fd5f3f0ee5a660416cacf7aefac6d137b2fd2ab8
Nicholas J Arnaud <s178066@students.aie.edu.au>
2018-02-28 12:01:09 -0600

    set variables with values

--- Parser.py
+++ Parser.py
@@ -1,24 +1,30 @@
-import os
+import random
+
 class Parser(object):
+    "Collects the string and seperates it into seperate strings to variablize"
     def __init__(self, expression):
         self.expression = expression
         self.variables = []
         self.vals = []
         self.pairs = []
+        self.value = 0
         self.grammar = ['*', '(', '+', ')', '!', '|', '^', '~']
 
     def parse(self):
-        self.setVariables()
+        "Parses through list"
+        self.initvariables()
         for i in self.variables:
             if i not in self.vals:
                 self.vals.append(i)
-        for rp in self.vals:
-            self.pairs.append((rp, ''))
-        print (self.variables)
-        print (self.vals)
-        print (self.pairs)
+        for r_p in self.vals:
+            self.value = random.randint(0, 1)
+            self.pairs.append((r_p, str(self.value)))
+        print self.variables
+        print self.vals
+        print self.pairs
 
-    def setVariables(self):
+    def initvariables(self):
+        "creates the variables"
         for index in self.expression:
             if(ord(index) >= 65) and (ord(index) <= 90):
                 index = chr(ord(index) + 32)
@@ -28,9 +34,10 @@ class Parser(object):
                 continue
             self.variables.append(index)
 
+
 if __name__ == '__main__':
-    string = Parser('(a+B)*(B+A)*(!d+E+F)')
-    string.parse()
+    STRING = Parser('(a+B)*(B+A)*(!d+E+F)')
+    STRING.parse()
 
 # parse (A+B)*(B+A)*(!D+E+F)
 # class Object
